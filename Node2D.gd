9996ecc308199749f5db81b9c5aaeae8ba6a069d
Clifford Decker <40838191+cliffman123@users.noreply.github.com>
2024-01-12 22:51:01 -0500

    Signal Collision Added

--- Node2D.gd
+++ Node2D.gd
@@ -1,16 +1,27 @@
 extends Node2D
 
-var radius = 10
+@onready var collision = $Area2D/CollisionShape2D
+@onready var sprite = $Area2D/Sprite2D
+
+@onready var radius_size = 10
 
 func _draw()->void:
-	draw_circle(Vector2(100,100), radius*1, Color.BLACK)
+	draw_circle(Vector2(0,0), radius_size*1, Color.BLACK)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#collision.get_shape().radius = 0
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	radius += delta*2
+	radius_size += delta*0.01
+	
 	queue_redraw()
+	
+	#collision.shape.radius += radius_size
 
+
+
+func _on_area_2d_area_entered(area):
+	print(area.name,"Detected")
